179b5e518f8b4945089091fba237058d5bc001cf
AyushTmg <ayushtmg20040722@gmail.com>
2024-08-10 20:45:08 +0545

    Feat: Added Button For User Logout

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,9 +1,23 @@
 import "./home.css"
+import ToastMessage from "../../utils/toaster/toaster"
+import { useNavigate } from "react-router-dom"
 
 export default function Home() {
+    const navigate = useNavigate()
+
+
+    const handleLogout = () => {
+        localStorage.clear()
+        ToastMessage.success("Successfully Logged Out")
+        return navigate('/login')
+    }
+
     return (
         <>
             <div>HomePage</div>
+            <button onClick={handleLogout}>Logout</button>
+            <button onClick={() => { navigate('/change-password') }}>Change Password</button>
+
         </>
     )
 }
